ac358ab40799d1d990689572db393d7eca622dbc
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-22 09:44:24 -0400

    [ADD] funcao para adicionar nova empresa

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -0,0 +1,24 @@
+package br.com.alura.gerenciador.web;
+
+import br.com.alura.gerenciador.Empresa;
+import br.com.alura.gerenciador.dao.EmpresaDAO;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresa extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String nome = req.getParameter("nome");
+		Empresa empresa = new Empresa(nome);
+		new EmpresaDAO().adiciona(empresa);
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><body>Empresa " + nome + " adicionada!</body></html>");
+	}
+}
