157c90c98f9633e2074b944609f5be3d5bb10c8a
Wellington Lopes <wellington.lopes.tw@gmail.com>
2018-02-22 11:54:25 -0400

    [ADJUST] separando html do java da pagina de logout

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -2,14 +2,13 @@ package br.com.alura.gerenciador.web;
 
 import br.com.alura.gerenciador.Usuario;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/logout")
 public class Logout extends HttpServlet {
@@ -20,8 +19,8 @@ public class Logout extends HttpServlet {
 		//ou invalidate tira tudo que estava na session
 		//req.getSession().invalidate();
 		//req.getSession().setMaxInactiveInterval(0);
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>Deslogado com sucesso</body></html>");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
 	}
 
 }
